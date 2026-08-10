343d9ba44b1c73a9c7e97356dbd00ef64f2066d9
cassandramazo-sys <cassandra_mazo@dlsu.edu.ph>
2026-08-10 22:20:18 +0800

    add Restaurant and Parking subclasses for all active guest tiers

--- src/main/java/com/aegisresort/facility/Parking.java
+++ src/main/java/com/aegisresort/facility/Parking.java
@@ -1,4 +1,15 @@
 package com.aegisresort.facility;
 
-public class Parking {
+import com.aegisresort.model.Guest;
+
+public class Parking extends Amenity {
+
+    public Parking(String facilityId, int maxCapacity) {
+        super(facilityId, "Resort Parking Lot", maxCapacity);
+    }
+
+    @Override
+    public boolean checkAccess(Guest guest) {
+        return guest != null && guest.isActive();
+    }
 }
