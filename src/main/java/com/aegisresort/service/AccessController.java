a491134cf7a4345ddc8911d945ff31d0e23108d2
cassandramazo-sys <cassandra_mazo@dlsu.edu.ph>
2026-08-10 22:21:54 +0800

    add AccessController interface for entry verification and audit logging

--- src/main/java/com/aegisresort/service/AccessController.java
+++ src/main/java/com/aegisresort/service/AccessController.java
@@ -1,4 +1,9 @@
 package com.aegisresort.service;
 
+import com.aegisresort.facility.Amenity;
+import com.aegisresort.model.Guest;
+
 public interface AccessController {
+    boolean verifyEntry(Guest guest, Amenity facility);
+    String generateAuditLog(Guest guest, Amenity facility);
 }
